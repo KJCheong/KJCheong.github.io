0a74d5e1b6caad5d980f419423bdb1a1ac62c09f
KJCheong <201429Z@mymail.nyp.edu.sg>
2022-08-10 23:18:32 +0800

    Fixed sum stuff
    Dream car page fixed abit

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -59,6 +59,11 @@ function Engineshow() {
   if (x.innerHTML === " ") {
     x.innerHTML = "<h3>Engine go crazy</h3><p>The Engine powering the R32 is a RB26DETT (6-cyl. In line, 4-valve DOHC + turbo) making about 2,568cc it can rev up to 6,800rpm making about 276 horse power and a 0-60 in about 5 seconds. However in the current day and age where aftermarket parts and other accessories become more widely available the power of this engine can be pushed to the higher ends of about 1000 horse power.</p>";
   }
+
+  var y = document.getElementById("TITLE");
+  if (y.innerHTML==" "){
+    y.innerHTML="<h1>Nissan R34 GTR</h1>"
+  } 
 }
 
 function Dimeshow() {
@@ -76,6 +81,11 @@ function Dimeshow() {
   if (x.innerHTML === " ") {
     x.innerHTML = "<h3>Dimensions go crazy</h3><p>The overall length,width and height is 4.6m,1,78m and 1.36m respectively and weighs 1560kgs which allows it to average 11.8 liters per kilometer which by todays standards is quite high but for a sports car of this calibre the kmpl is already conidered quite normal.</p>";
   }
+
+  var y = document.getElementById("TITLE");
+  if (y.innerHTML==" "){
+    y.innerHTML="<h1>Nissan R34 GTR</h1>"
+  } 
 }
 
 function Wheelshow() {
@@ -91,6 +101,11 @@ function Wheelshow() {
   if (x.innerHTML === " ") {
     x.innerHTML = "<h3>Wheels go crazy</h3><p>The wheels of a car may not seem that important but fitting it with the right wheels and tires could mean the difference between driving smoothly and skidding off the road. The R34 takes 245/40ZR18 wheels in both the front and rear of the car, broken down this would mean that the car has 245mm width tires and has a 40% aspect ratio of height to width, the ZR is means that is has a pseed rating of over 240km/h and finally the 18 tells us the diameter of the tire is 18 inches.</p>";
   }
+
+  var y = document.getElementById("TITLE");
+  if (y.innerHTML==" "){
+    y.innerHTML="<h1>Nissan R34 GTR</h1>"
+  } 
 }
 
 function Inteshow() {
@@ -106,6 +121,11 @@ function Inteshow() {
   if (x.innerHTML === " ") {
     x.innerHTML = "<h3>Interior go crazy</h3> <p>Back when this car was still new, the fancy gizmos and technology on the dashboard looked like something out of a movie, but now can seem quite tacky and obsolete. It features things like lap timers oil temperature gauges and all sorts of things. However, no one has ever bought a GTR for its interior. The car is more function than form. The car is meant to be more driver oriented, so the seats in front will naturally have bigger room than the back. The vision in the car is also good with the only downside being the bisected rear view due to the spoiler.</p>";
   }
+
+  var y = document.getElementById("TITLE");
+  if (y.innerHTML==" "){
+    y.innerHTML="<h1>Nissan R34 GTR</h1>"
+  } 
 }
 
 
